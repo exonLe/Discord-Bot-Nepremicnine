636a880895989ff3f99fe380109ba8cdb2ef533a
Bojan Faletic <bojanf.1996@gmail.com>
2022-01-31 00:16:22 +0100

    formated all files

--- data_pull.py
+++ data_pull.py
@@ -4,52 +4,55 @@ from bs4 import BeautifulSoup
 
 # funkcija ki vrne točno določeno vrstico s strani
 def nepremicne_web():
-    url = 'https://www.nepremicnine.net/oglasi-oddaja/ljubljana-mesto/stanovanje/'
+    url = "https://www.nepremicnine.net/oglasi-oddaja/ljubljana-mesto/stanovanje/"
     response = requests.get(url)
-    print(response) #If response==200 -> OK
+    print(response)  # If response==200 -> OK
 
     soup = BeautifulSoup(response.text, "html.parser")
-    one_a_tag = soup.findAll('a')[170] ##returns 170 apartment
-    #all_tag = soup.findAll('a') #For testing ##find all hyperlinks<a>
-    link = one_a_tag['href']
-    print(link) #Check the path and add it below (download_url...)
+    one_a_tag = soup.findAll("a")[170]  ##returns 170 apartment
+    # all_tag = soup.findAll('a') #For testing ##find all hyperlinks<a>
+    link = one_a_tag["href"]
+    print(link)  # Check the path and add it below (download_url...)
 
-    download_url = 'https://www.nepremicnine.net/'+link
-    return download_url #Returns link
+    download_url = "https://www.nepremicnine.net/" + link
+    return download_url  # Returns link
+
 
 # debug: nepremicne_web()
 
 
 # funkcija, ki poišče vse oglase (div->seznam->h2->data-href)
 def Neprweb():
-    url = 'https://www.nepremicnine.net/oglasi-oddaja/ljubljana-mesto/stanovanje/?s=16'
+    url = "https://www.nepremicnine.net/oglasi-oddaja/ljubljana-mesto/stanovanje/?s=16"
     response = requests.get(url)
 
     soup = BeautifulSoup(response.text, "html.parser")
-    container = soup.find('div', class_= 'seznam')
+    container = soup.find("div", class_="seznam")
 
-    for link in container.findAll('h2'):
-        #print(link.get('data-href'))
-        addlink = link.get('data-href')
-        print('https://www.nepremicnine.net'+addlink)
-        #time.sleep(1)
+    for link in container.findAll("h2"):
+        # print(link.get('data-href'))
+        addlink = link.get("data-href")
+        print("https://www.nepremicnine.net" + addlink)
+        # time.sleep(1)
+
 
 # debug: Neprweb()
 
 
 # funkcija, ki poišče zadnji oglas (div->seznam->h2->data-href)
 def Neprweb1():
-    url = 'https://www.nepremicnine.net/oglasi-oddaja/ljubljana-mesto/stanovanje/?s=16'
+    url = "https://www.nepremicnine.net/oglasi-oddaja/ljubljana-mesto/stanovanje/?s=16"
     response = requests.get(url)
 
     soup = BeautifulSoup(response.text, "html.parser")
-    container = soup.find('div', class_= 'seznam')
+    container = soup.find("div", class_="seznam")
 
-    for link in container.findAll('h2')[:1]:
-        #print(link.get('data-href'))
-        addlink = link.get('data-href')
-        #print('https://www.nepremicnine.net'+addlink)
-        download_url = 'https://www.nepremicnine.net/'+addlink
+    for link in container.findAll("h2")[:1]:
+        # print(link.get('data-href'))
+        addlink = link.get("data-href")
+        # print('https://www.nepremicnine.net'+addlink)
+        download_url = "https://www.nepremicnine.net/" + addlink
         return download_url
 
-#debug: Neprweb1()
+
+# debug: Neprweb1()
--- nepremicnine_bot_discord.py
+++ nepremicnine_bot_discord.py
@@ -1,19 +1,31 @@
-import discord
+try:
+    from discord import Client
+except ModuleNotFoundError:
+    class Client:
+        def run(self, _):
+            print("ERROR: Unable to import discord Client")
+            return _
+
+
 import asyncio
 
 from data_pull import Neprweb1
 from read_DB import read_link
 from insert_and_update_DB import updateSqliteTable
+import os
+
+TOKEN = ""
+if "TOKEN" in os.environ:
+    TOKEN = os.environ["TOKEN"]
+if TOKEN is None:
+    print("ERROR: insert token or pass by env variable")
 
 
-TOKEN = ''
-
-
-class MyClient(discord.Client):
-    ''' ######  MAKE BOT ONLINE ####### '''
+class MyClient(Client):
+    """######  MAKE BOT ONLINE #######"""
 
     async def on_ready(self):
-        print(f'{client.user} is now online!')
+        print(f"{client.user} is now online!")
 
     async def on_message(self, message):
         # Don't respond to ourselves - we don't want to loop bot
@@ -23,35 +35,43 @@ class MyClient(discord.Client):
         message_content = message.content.lower()
 
         # Send bot info for options
-        if message.content == 'info':
-            await message.channel.send('options: $realtime, nepremicnine, stanovanje')
+        if message.content == "info":
+            await message.channel.send("options: $realtime, nepremicnine, stanovanje")
 
         # Send bot nepremicnine for general real-estate link
-        if message.content == 'nepremicnine':
-            await message.channel.send('https://www.nepremicnine.net/oglasi-oddaja/ljubljana-mesto/stanovanje/')
+        if message.content == "nepremicnine":
+            await message.channel.send(
+                "https://www.nepremicnine.net/oglasi-oddaja/ljubljana-mesto/stanovanje/"
+            )
 
         # Get from data_pull, the last apartment that was published
-        if message.content == 'stanovanje':
+        if message.content == "stanovanje":
             zadnje_stanovanje = Neprweb1()
-            if zadnje_stanovanje != read_link(): #Check if link is the same
-                updateSqliteTable(1, zadnje_stanovanje) #Update table
-            await message.channel.send(zadnje_stanovanje) #Send a message back to the user in the same channel
+            if zadnje_stanovanje != read_link():  # Check if link is the same
+                updateSqliteTable(1, zadnje_stanovanje)  # Update table
+            await message.channel.send(
+                zadnje_stanovanje
+            )  # Send a message back to the user in the same channel
         ######################
 
-        if '$realtime' in message_content: #Write $realtime to chat bot, to get realtime feedback
+        if (
+            "$realtime" in message_content
+        ):  # Write $realtime to chat bot, to get realtime feedback
             while True:
                 zadnje_stanovanje = Neprweb1()
-                if zadnje_stanovanje != read_link(): #Check if link is the same
-                    updateSqliteTable(1, zadnje_stanovanje) #Update table
-                    await message.channel.send('nova nepremicnina link')
-                    await message.channel.send(zadnje_stanovanje) #Send a message back to the user in the same channel
+                if zadnje_stanovanje != read_link():  # Check if link is the same
+                    updateSqliteTable(1, zadnje_stanovanje)  # Update table
+                    await message.channel.send("nova nepremicnina link")
+                    await message.channel.send(
+                        zadnje_stanovanje
+                    )  # Send a message back to the user in the same channel
                     print("nova nepremicnina link \n")
                 await asyncio.sleep(600)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     client = MyClient()
     client.run(TOKEN)
 
-##vsako uro (1h) naredi request in pogleda, če je link, ki ga dobi isti(pogleda v bazo-sqllite), (if) če je isti ne naredi nič, če je nov, ga pošlje.
-
+# vsako uro (1h) naredi request in pogleda, če je link, ki ga dobi isti(pogleda v bazo-sqllite), \
+# (if) če je isti ne naredi nič, če je nov, ga pošlje.
